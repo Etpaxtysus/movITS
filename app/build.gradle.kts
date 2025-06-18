81b187fc4a0c67192b6cd042a69cb0f5df96d4a0
Etpaxtysus <itsrhenaldy@gmail.com>
2025-06-18 18:42:33 +0700

    fix: add missing depenencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -58,9 +59,9 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    implementation(libs.firebase.bom)
+    implementation(platform(libs.firebase.bom))
     implementation (libs.androidx.navigation.compose)
-    implementation (libs.androidx.lifecycle.runtime.ktx.v262)
+    implementation (libs.androidx.lifecycle.runtime.ktx)
     implementation (libs.firebase.auth.ktx)
     implementation (libs.play.services.auth)
 
